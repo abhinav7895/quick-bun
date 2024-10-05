6cb35b05dc4b927569473e783a949a67bdf4af34
abhinav7895 <abhinavay2003@gmail.com>
2024-10-05 12:48:43 +0530

    Refactor middleware.ts to handle authentication and authorization

---
+++ src/middleware.ts
@@ -0,0 +1,27 @@
+import { auth } from "@/auth";
+import { NextResponse } from "next/server";
+
+export default auth((req) => {
+  const isAuthenticated = !!req.auth;
+  const pathname = req.nextUrl.pathname;
+
+
+  if (!isAuthenticated && !pathname.startsWith("/signin")) {
+    return NextResponse.redirect(new URL("/signin", req.url));
+  }
+
+  // If the user is authenticated and on the signin page or root, redirect to home
+  if (isAuthenticated && (pathname === "/signin")) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+
+  // For all other cases, allow the req to proceed
+  return NextResponse.next();
+});
+
+export const config = {
+  matcher: [
+    "/signin",
+    "/"
+  ],
+};
